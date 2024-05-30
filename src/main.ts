f19bc65bd97041fdbbe2cf4e19ac0c357fc608c2
RFradioFactory <nevstruev.2004@mail.ru>
2024-05-30 20:53:47 +0500

    gg

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppComponent } from './app/app.component';
 import { HttpClient, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AuthInterceptor } from './app/service/auth.interceptor';
 import { MainPageComponent } from './app/components/main-page/main-page.component';
-
+//git commit -a -m 'some message'
   
 
 bootstrapApplication(AppComponent, appConfig)
